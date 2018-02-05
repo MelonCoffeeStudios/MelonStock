aec96bc1fc3d67e8ffe1a6da14e77cee0350196e
Aaron <aaronjgriffin96@gmail.com>
2018-02-05 17:35:54 +0000

    Stopped annoying warning from mongoose

--- app.js
+++ app.js
@@ -27,7 +27,7 @@
 
 	var configDB = require('./config/database.js');
 	// configuration ===============================================================
-	mongoose.connect(configDB.url); // connect to our database
+	mongoose.connect(configDB.url, {useMongoClient:true}); // connect to our database
 
 	require('./config/passport')(passport); // pass passport for configuration
  // App Settings
